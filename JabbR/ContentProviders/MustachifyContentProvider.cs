47ef09f36acb6f1c3cd4ec8f1fc3ec70464f6d83
petegoo <pete@petegoo.com>
2013-01-29 23:26:07 +1300

    Added mustachify content provider

---
+++ JabbR/ContentProviders/MustachifyContentProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using JabbR.ContentProviders.Core;
+
+namespace JabbR.ContentProviders {
+    public class MustachifyContentProvider : CollapsibleContentProvider
+    {
+        protected override Task<ContentProviderResult> GetCollapsibleContent(ContentProviderHttpRequest request)
+        {
+            return TaskAsyncHelper.FromResult(new ContentProviderResult()
+            {
+                Content = String.Format(@"<img src=""{0}"" />", request.RequestUri),
+                Title = request.RequestUri.AbsoluteUri.ToString()
+            });
+        }
+
+        public override bool IsValidContent(Uri uri)
+        {
+            return uri.AbsoluteUri.StartsWith("http://mustachify.me/", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
