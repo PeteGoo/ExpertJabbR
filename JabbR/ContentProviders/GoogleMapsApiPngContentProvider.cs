105accdcea0efa52eec3e2eae2aa5cfdd85f2028
petegoo <pete@petegoo.com>
2013-01-29 23:32:31 +1300

    Added Google Maps Api content provider

---
+++ JabbR/ContentProviders/GoogleMapsApiPngContentProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using JabbR.ContentProviders.Core;
+
+namespace JabbR.ContentProviders {
+    public class GoogleMapsApiPngContentProvider : CollapsibleContentProvider
+    {
+        protected override Task<ContentProviderResult> GetCollapsibleContent(ContentProviderHttpRequest request)
+        {
+            return TaskAsyncHelper.FromResult(new ContentProviderResult()
+            {
+                Content = String.Format(@"<img src=""{0}"" />", request.RequestUri),
+                Title = request.RequestUri.AbsoluteUri.ToString()
+            });
+        }
+
+        public override bool IsValidContent(Uri uri)
+        {
+            return uri.AbsoluteUri.StartsWith("http://maps.google.com/maps/api/", StringComparison.OrdinalIgnoreCase) &&  uri.AbsoluteUri.IndexOf("format=png", StringComparison.OrdinalIgnoreCase) > -1;
+        }
+    }
+}
